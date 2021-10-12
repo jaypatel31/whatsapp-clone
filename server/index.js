3b0baf295c941d70e761adfce28a187ce310c4d9
Jay Patel <jaypatel32157@gmail.com>
2021-10-12 14:38:22 +0530

    Heroku Deploy

--- server/index.js
+++ server/index.js
@@ -18,7 +18,16 @@ const MONGO_URI = process.env.MONGOURI
 
 Connection(MONGO_URI);
 
-const PORT = 5000;
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('frontend/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'../frontend','build','index.html'))
+    })
+}
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`)
 })
